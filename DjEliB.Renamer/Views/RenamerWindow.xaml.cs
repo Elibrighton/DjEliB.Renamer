dadc27b4db327a0647896ff3ef42a9fa85fc303f
Eli Brighton <st3gs@hotmail.com>
2018-04-02 14:14:38 +1000

    Change StartUpUri to new window

--- DjEliB.Renamer/Views/RenamerWindow.xaml.cs
+++ DjEliB.Renamer/Views/RenamerWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DjEliB.Renamer
+namespace DjEliB.Renamer.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RenamerWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RenamerWindow : Window
     {
-        public MainWindow()
+        public RenamerWindow()
         {
             InitializeComponent();
         }
