d133273870c74908cd7ed5d8487e6f1921510f50
Eli Brighton <st3gs@hotmail.com>
2018-04-04 22:00:38 +1000

    Implement RelayCommand

--- DjEliB.Renamer/ViewModels/RenamerViewModel.cs
+++ DjEliB.Renamer/ViewModels/RenamerViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace DjEliB.Renamer.ViewModels
@@ -12,6 +13,7 @@ namespace DjEliB.Renamer.ViewModels
     public class RenamerViewModel : ObservableObject
     {
         private RenamerModel Renamer = new RenamerModel();
+
         private string _txtSourceDirectory;
 
         public string TxtSourceDirectory
@@ -23,15 +25,100 @@ namespace DjEliB.Renamer.ViewModels
                 RaisePropertyChangedEvent("TxtSourceDirectory");
             }
         }
+        
+        private bool _chkbxIsSinglesChecked;
 
-        public ICommand BrowseButtonClickCommand
+        public bool ChkbxIsSinglesChecked
         {
-            get { return new DelegateCommand(GetSourceDirectory); }
+            get { return _chkbxIsSinglesChecked; }
+            set
+            {
+                _chkbxIsSinglesChecked = value;
+                RaisePropertyChangedEvent("ChkbxIsSinglesChecked");
+            }
         }
+
+        private bool _chkbxIsElectroHouseChecked;
+
+        public bool ChkbxIsElectroHouseChecked
+        {
+            get { return _chkbxIsElectroHouseChecked; }
+            set
+            {
+                _chkbxIsElectroHouseChecked = value;
+                RaisePropertyChangedEvent("ChkbxIsElectroHouseChecked");
+            }
+        }
+
+        private bool _chkbxIsDjFtpChecked;
+
+        public bool ChkbxIsDjFtpChecked
+        {
+            get { return _chkbxIsDjFtpChecked; }
+            set
+            {
+                _chkbxIsDjFtpChecked = value;
+                RaisePropertyChangedEvent("ChkbxIsDjFtpChecked");
+            }
+        }
+
+        private bool _chkbxIsUnderscoreChecked;
+
+        public bool ChkbxIsUnderscoreChecked
+        {
+            get { return _chkbxIsUnderscoreChecked; }
+            set
+            {
+                _chkbxIsUnderscoreChecked = value;
+                RaisePropertyChangedEvent("ChkbxIsUnderscoreChecked");
+            }
+        }
+
+        private bool _chkbxIsSelectAllChecked;
+
+        public bool ChkbxIsSelectAllChecked
+        {
+            get {
+                return _chkbxIsSelectAllChecked;
+            }
+            set
+            {
+                _chkbxIsSelectAllChecked = value;
+                RaisePropertyChangedEvent("ChkbxIsSelectAllChecked");
+            }
+        }
+
+        private ICommand _getSourceDirectoryCommand;
+
+        public ICommand GetSourceDirectoryCommand
+        {
+            get
+            {
+                return _getSourceDirectoryCommand ?? (_getSourceDirectoryCommand = new RelayCommand(x => {GetSourceDirectory();}));
+            }
+        }
+
+        //private ICommand _selectAllCommand;
+
+        //public ICommand SelectAllCommand
+        //{
+        //    get
+        //    {
+        //        return _selectAllCommand ?? (_selectAllCommand = new RelayCommand(x => { SelectAll(); }));
+        //    }
+        //}
 
         public void GetSourceDirectory()
         {
             TxtSourceDirectory = Renamer.GetSourceDirectory();
         }
+
+        //public void SelectAll()
+        //{
+        //    if (!_chkbxIsSinglesChecked)
+        //    {
+        //        ChkbxIsSinglesChecked = true;
+        //    }
+        //}
     }
 }
